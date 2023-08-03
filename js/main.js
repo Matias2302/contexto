6b50e571bfaeeb274d9a652ecd41decc5a5f12ca
Matias2302 <matias.23.02@gmail.com>
2023-08-03 18:18:59 -0300

    cambio en hero y texto al presionar boton

--- js/main.js
+++ js/main.js
@@ -14,3 +14,36 @@ navToggle.addEventListener('click', () => {
 
     }
 });
+
+/*===Contenido dinámico con trigger====*/
+// Obtener el botón y el div con el texto adicional
+const mostrarBtn = document.getElementById("mostrarBtn");
+const textoAdicional = document.getElementById("texto-adicional");
+const hero = document.getElementById("hero");
+const box = document.getElementById("box");
+const contextoLogo = document.getElementById("contexto-logo");
+
+// Agregar un evento de clic al botón
+mostrarBtn.addEventListener("click", function () {
+  // Mostrar o ocultar el div con el texto adicional
+  if (textoAdicional.style.display === "none") 
+  {
+    textoAdicional.style.display = "block";
+    textoAdicional.classList.remove("fade-out");
+    textoAdicional.classList.add("fade-in");
+    textoAdicional.classList.add("texto-adicional-full");
+    box.classList.add("box-1");
+    hero.classList.add("hero-texto");
+    contextoLogo.classList.add("contexto-logo-mid");
+    
+    mostrarBtn.textContent = "Menos info";
+  } else {
+    textoAdicional.classList.remove("fade-in");
+    textoAdicional.classList.add("fade-out");
+    box.classList.remove("box-1");
+    hero.classList.remove("hero-texto");
+    contextoLogo.classList.remove("contexto-logo-mid");
+    textoAdicional.style.display = "none";
+    mostrarBtn.textContent = "Más info";
+  }
+});
